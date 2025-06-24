cc6373520ab1d32a2387ad587469db0d5e2d7a3f
Melkkior <salesmelchiosjoaovitor@gmail.com>
2025-06-24 16:39:24 -0400

    ligando a pagina em outras

--- php/PaginaEH.php
+++ php/PaginaEH.php
@@ -1,230 +1,242 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <title>Horário - Segunda-feira</title>
-  <link rel="stylesheet" href="materias/css/EH.css">
+  <link rel="stylesheet" href="materias/editados/css/EH.css">
 </head>
+
 <body>
-  <div class="container"><center><h1>editar horario</h1></center></div>
+  <div class="r">
+    <div class="esquerda"><input class="butal" type="button" value="IR PARA TELA DE HORÁRIO" onclick="location.href='horarios/PaginaSegunda.php'">
+    </div>
+    <div class="direita"><img src="https://cdn-icons-png.flaticon.com/512/4277/4277132.png" alt=""></div>
+  </div>
   <form action="">
     <div class="container">
-    <div class="header">SEGUNDA</div>
-    <table>
-      <tr>
-        <th></th>
-        <th>7:10</th>
-        <th>7:50</th>
-        <th>8:50</th>
-        <th>9:50</th>
-        <th>10:50</th>
-        <th>11:40</th>
-        <th>13:00</th>
-        <th>13:50</th>
-        <th>14:40</th>
-        <th>15:00</th>
-        <th>15:50</th>
-        <th>16:40</th>
-      </tr>
-      <!-- ADM 1 -->
-      <tr>
-        <td class="row-title">ADM 1</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- ADM 2 -->
-      <tr>
-        <td class="row-title">ADM 2</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- ADM 3 -->
-      <tr>
-        <td class="row-title">ADM 3</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- AGRO 1 -->
-      <tr>
-        <td class="row-title">AGRO 1</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- AGRO 2 -->
-      <tr>
-        <td class="row-title">AGRO 2</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- AGRO 3 -->
-      <tr>
-        <td class="row-title">AGRO 3</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- INFO 1 -->
-      <tr>
-        <td class="row-title">INFO 1</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- INFO 2 -->
-      <tr>
-        <td class="row-title">INFO 2</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- INFO 3 -->
-      <tr>
-        <td class="row-title">INFO 3</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- MEIO 1 -->
-      <tr>
-        <td class="row-title">MEIO 1</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- MEIO 2 -->
-      <tr>
-        <td class="row-title">MEIO 2</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-      <!-- MEIO 3 -->
-      <tr>
-        <td class="row-title">MEIO 3</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-        <td class="break">---</td>
-        <td><input class="input" type="text"></td>
-        <td><input class="input" type="text"></td>
-      </tr>
-    </table>
-    <div class="footer">
-      AVISO(S): <input class="input" type="text">
+      <div class="header">SEGUNDA</div>
+      <table>
+        <tr>
+          <th></th>
+          <th>7:10</th>
+          <th>7:50</th>
+          <th>8:50</th>
+          <th>9:50</th>
+          <th>10:50</th>
+          <th>11:40</th>
+          <th>13:00</th>
+          <th>13:50</th>
+          <th>14:40</th>
+          <th>15:00</th>
+          <th>15:50</th>
+          <th>16:40</th>
+        </tr>
+        <!-- ADM 1 -->
+        <tr>
+          <td class="row-title">ADM 1</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- ADM 2 -->
+        <tr>
+          <td class="row-title">ADM 2</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- ADM 3 -->
+        <tr>
+          <td class="row-title">ADM 3</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- AGRO 1 -->
+        <tr>
+          <td class="row-title">AGRO 1</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- AGRO 2 -->
+        <tr>
+          <td class="row-title">AGRO 2</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- AGRO 3 -->
+        <tr>
+          <td class="row-title">AGRO 3</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- INFO 1 -->
+        <tr>
+          <td class="row-title">INFO 1</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- INFO 2 -->
+        <tr>
+          <td class="row-title">INFO 2</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- INFO 3 -->
+        <tr>
+          <td class="row-title">INFO 3</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- MEIO 1 -->
+        <tr>
+          <td class="row-title">MEIO 1</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- MEIO 2 -->
+        <tr>
+          <td class="row-title">MEIO 2</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+        <!-- MEIO 3 -->
+        <tr>
+          <td class="row-title">MEIO 3</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+          <td class="break">---</td>
+          <td><input class="input" type="text"></td>
+          <td><input class="input" type="text"></td>
+        </tr>
+      </table>
+      <div class="footer">
+        AVISO(S): <input class="input" type="text">
+      </div>
+      <br>
+
     </div>
-    <br>
-    <center><input type="submit" value="Salvar" class="button"></center>
-  </div>
+    <div class="f">
+      <input type="button" class="botao" value="EDITAR BANCO DE DADOS" onclick="location.href='materias/PaginaEditarM.php'">
+      <input type="submit" class="botao" value="SALVAR ALTERAÇÕES">
+      
+    </div>
   </form>
 </body>
+
 </html>
