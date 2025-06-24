e162cd53c3b2e891d77e0987ee1e2b1cdf1deda5
Melkkior <salesmelchiosjoaovitor@gmail.com>
2025-06-24 16:04:32 -0400

    atualizações que eu esqueci quais foram :p

--- php/materias/turma.php
+++ php/materias/turma.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Cadastro de Matérias</title>
+    <title>Cadastro de Turma</title>
     <link rel="stylesheet" href="css/disciplina.css">
 </head>
 
@@ -15,17 +15,15 @@
         </div>
 
         <form action="salvar.php" method="POST" id="formulario">
-            <h2>Cadastro de Matérias</h2>
+            <h2>Cadastro de Turma</h2>
             <div id="campos">
                 <div class="box">
-                    <label>Matéria: <input type="text" name="materia[]"></label>
-                    <label>Professor: <input type="text" name="professor[]"></label>
-                    Curso:
+                    <label>Turma: <input type="text" name="materia[]"></label>
+                    ano:
                     <div class="skibid">
-                        <label><input type="radio" name="curso[0]" value="info"> Info</label>
-                        <label><input type="radio" name="curso[0]" value="agro"> Agro</label>
-                        <label><input type="radio" name="curso[0]" value="meio"> Meio</label>
-                        <label><input type="radio" name="curso[0]" value="adm"> Adm</label>
+                        <label><input type="radio" name="curso[0]" value="info"> 1º ano</label>
+                        <label><input type="radio" name="curso[0]" value="agro"> 2º ano</label>
+                        <label><input type="radio" name="curso[0]" value="meio"> 3º ano</label>
                     </div>
                 </div>
             </div>
